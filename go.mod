9ce626635041a53ebaf5defa06e7190a0af20f95
pis0sion <zihuang2010@126.com>
2022-01-28 20:03:07 +0800

    add validator components commit @pis0sion

--- go.mod
+++ go.mod
@@ -9,11 +9,12 @@ require (
 )
 
 require (
+	github.com/Pis0sion/components v1.0.0 // indirect
 	github.com/golang/protobuf v1.5.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.4 // indirect
 	golang.org/x/net v0.0.0-20200822124328-c89045814202 // indirect
-	golang.org/x/sys v0.0.0-20200323222414-85ca7c5b95cd // indirect
-	golang.org/x/text v0.3.0 // indirect
+	golang.org/x/sys v0.0.0-20210806184541-e5e7981a1069 // indirect
+	golang.org/x/text v0.3.6 // indirect
 	google.golang.org/genproto v0.0.0-20200526211855-cb27e3aa2013 // indirect
 )
--- store/rblog/v1/article.go
+++ store/rblog/v1/article.go
@@ -1,24 +1,39 @@
 package v1
 
-import metav1 "github.com/Pis0sion/rblogrus/meta/types"
+import (
+	"github.com/Pis0sion/components/validate"
+	metav1 "github.com/Pis0sion/rblogrus/meta/types"
+)
+
+type unifiedValidator interface {
+	Validate() error
+}
 
 type Article struct {
-	metav1.ObjectMeta `json:"metadata,omitempty"`
-
+	metav1.ObjectMeta  `json:"metadata,omitempty"`
 	AuthorID           uint64 `json:"authorID"     gorm:"column:authorID" validate:"omitempty"`
 	ArticleTitle       string `json:"articleTitle" gorm:"column:articleTitle" validate:"omitempty"`
 	ArticleContent     string `json:"articleContent"  gorm:"column:articleContent" validate:"omitempty"`
 	ArticleDescription string `json:"articleDescription"  gorm:"column:articleDescription" validate:"omitempty"`
-	Status             int    `json:"Status"    gorm:"column:Status" validate:"omitempty"`
+	Status             int    `json:"status"    gorm:"column:Status" validate:"omitempty"`
 }
 
 type ArticleList struct {
-
 	metav1.ListMeta `json:",inline"`
-
-	Items []*Article `json:"items"`
+	Items           []*Article `json:"items"`
 }
 
 func (a *Article) TableName() string {
 	return "article"
+}
+
+type Art2Create struct {
+	AuthorID           uint64 `json:"authorID"  validate:"required"`
+	ArticleTitle       string `json:"articleTitle"  validate:"required,min=1,max=100"`
+	ArticleContent     string `json:"articleContent"  validate:"required,min=1"`
+	ArticleDescription string `json:"articleDescription" validate:"omitempty"`
+}
+
+func (c *Art2Create) Validate() error {
+	return validate.NewValidator(c).Validate()
 }
