1f3eeaccccffa2c6fa48fb64d4fddc066fda05aa
pis0sion <zihuang2010@126.com>
2022-01-28 12:01:46 +0800

    initialize rblogrus components commit @pis0sion

---
+++ meta/types/meta.go
@@ -0,0 +1,12 @@
+package types
+
+import "time"
+
+type ObjectMeta struct {
+	// Primary key
+	ID uint64 `json:"id,omitempty" gorm:"primary_key;AUTO_INCREMENT;column:id"`
+	// createAt
+	CreatedAt time.Time `json:"createdAt,omitempty" gorm:"column:createdAt"`
+	// updateAt
+	UpdatedAt time.Time `json:"updatedAt,omitempty" gorm:"column:updatedAt"`
+}
---
+++ store/rblog/v1/secret.go
@@ -0,0 +1,24 @@
+package v1
+
+import (
+	metaV1 "github.com/Pis0sion/rblogrus/meta/types"
+	"time"
+)
+
+type Secret struct {
+
+	metaV1.ObjectMeta `json:"metadata,omitempty"`
+
+	AppName         string    `json:"appName"        gorm:"column:appName"  validate:"omitempty"`
+	InstanceID      int       `json:"instanceID"     gorm:"column:instanceID"  validate:"omitempty"`
+	AppAccessKey    string    `json:"appAccessKey"   gorm:"column:appAccessKey"  validate:"omitempty"`
+	AppAccessSecret string    `json:"appAccessSecret" gorm:"column:appAccessSecret"  validate:"omitempty"`
+	Description     string    `json:"description"    gorm:"column:description"  validate:"omitempty"`
+	ExpireTime      time.Time `json:"expireTime"     gorm:"column:expireTime"  validate:"omitempty"`
+}
+
+func (s Secret) TableName() string {
+	return "secret"
+}
+
+
